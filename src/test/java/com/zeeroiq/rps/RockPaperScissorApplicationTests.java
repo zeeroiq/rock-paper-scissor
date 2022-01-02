8a8081e1e761f2a810b78c27e0e85ff140fdf92c
zeeroiq <shrikantvishwakarma@outlook.com>
2022-01-03 01:33:39 +0530

    project initialization files. closes #1

---
+++ src/test/java/com/zeeroiq/rps/RockPaperScissorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeeroiq.rps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
